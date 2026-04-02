3bc6ca84120179f7c0a7c996bad65640f2c5c81c
Rebecka94 <rebeckalarsson.1994@gmail.com>
2026-04-02 19:16:12 +0200

    Add Navbar component and associated styles; remove unused code from App

---
+++ src/components/layout/Navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.container}>
+        <ul className={styles.links}>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,22 +1,10 @@
-import { Link } from "react-router-dom";
-import Button from "../components/ui/Button/Button";
-import Text from "../components/ui/Text/Text";
+import Navbar from "../components/layout/Navbar/Navbar"
 
 export default function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>h1</h1>
-      <h2>h2</h2>
-      <h3>h3</h3>
-
-      <Text size="medium">Text MD</Text>
-      <Text size="small">Text SM</Text>
-      <nav>
-        <Link to="/about">About</Link>
-        <Button variant="primary" size="sm">Small</Button>
-        <Button variant="primary" size="md">Medium</Button>
-        <Button size="lg" variant="secondary">large</Button>
-      </nav>
     </div>
   );
 }
