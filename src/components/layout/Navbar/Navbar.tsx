bf2f80b323898c146f8e618f70ce1ae2f9ff8b2c
Rebecka94 <rebeckalarsson.1994@gmail.com>
2026-05-04 15:42:10 +0200

    Update Navbar styles and structure: adjust link alignment, spacing, and button integration

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -1,16 +1,23 @@
 import { Link } from "react-router-dom";
+import Button from "../../ui/Button/Button";
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
-  <div className="container">
-    <ul className={styles.links}>
-      <li><Link to="/about">About</Link></li>
-      <li><Link to="/projects">Projects</Link></li>
-      <li><Link to="/contact">Contact</Link></li>
-    </ul>
-  </div>
-</nav>
+      <div className="container">
+        <ul className={styles.links}>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <Button variant="outlined" size="sm" style={{ fontSize: "20px" }}>
+            Contact
+          </Button>
+        </ul>
+      </div>
+    </nav>
   );
 }
