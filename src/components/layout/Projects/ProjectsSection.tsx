7bebcd917ffc2de5f597d77ce1400f85d04a7c4c
Rebecka94 <rebeckalarsson.1994@gmail.com>
2026-05-18 17:54:23 +0200

    Add container styles to ProjectsSection for improved layout

--- src/components/layout/Projects/ProjectsSection.tsx
+++ src/components/layout/Projects/ProjectsSection.tsx
@@ -1,6 +1,8 @@
+import styles from "./ProjectsSection.module.css";
+
 export default function ProjectsSection() {
   return (
-    <div id="projects">
+    <div id="projects" className={styles.container}>
       <h2>My projects</h2>
       <p>List of projects ↓</p>
     </div>
