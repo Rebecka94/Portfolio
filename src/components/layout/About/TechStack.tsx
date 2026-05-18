e2d35c7d60259f843a0a51789bf8b01a0dba87d8
Rebecka94 <rebeckalarsson.1994@gmail.com>
2026-05-18 17:54:48 +0200

    Refactor TechStack component to dynamically render technology items and update styles

--- src/components/layout/About/TechStack.tsx
+++ src/components/layout/About/TechStack.tsx
@@ -1,15 +1,36 @@
 import styles from "./TechStack.module.css";
 
+const technologies = [
+  { name: "HTML", image: "HTML5.png" },
+  { name: "CSS", image: "CSS3.png" },
+  { name: "JavaScript", image: "JavaScript.png" },
+  { name: "TypeScript", image: "TypeScript.png" },
+  { name: "React", image: "React.png" },
+  { name: "Next.js", image: "Next.js.png" },
+  { name: "Node.js", image: "Node.js.png" },
+  { name: "Git", image: "Git.png" },
+  { name: "Azure DevOps", image: "Azure-Devops.png" },
+  { name: "GitHub", image: "GitHub.png" },
+  { name: "Figma", image: "Figma.png" },
+  { name: "Illustrator", image: "Adobe-Illustrator.png" },
+];
+
 export default function TechStack() {
   return (
     <div className={styles.container}>
       <h3>Tech Stack</h3>
-      <ul className={styles.techList}>
-        <li>React</li>
-        <li>JavaScript</li>
-        <li>TypeScript</li>
-        <li>CSS Modules</li>
-      </ul>
+      <div className={styles.techList}>
+        {technologies.map((tech) => (
+          <div key={tech.name} className={styles.techcontainer}>
+            <img
+              src={`/images/tech/${tech.image}`}
+              alt={tech.name}
+              className={styles.icon}
+            />
+            <span>{tech.name}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
