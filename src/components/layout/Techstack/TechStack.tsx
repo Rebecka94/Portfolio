0eb3a89ead474e7e6d70eed53d7fd60bd0f1a993
Rebecka94 <rebeckalarsson.1994@gmail.com>
2026-06-29 10:35:28 +0200

    Group TechStack items by category with labeled sections

--- src/components/layout/Techstack/TechStack.tsx
+++ src/components/layout/Techstack/TechStack.tsx
@@ -1,37 +1,61 @@
-import Text from "../../ui/Text/Text";
 import styles from "./TechStack.module.css";
 
-const technologies = [
-  { name: "HTML", image: "HTML5.png" },
-  { name: "CSS", image: "CSS3.png" },
-  { name: "JavaScript", image: "JavaScript.png" },
-  { name: "TypeScript", image: "TypeScript.png" },
-  { name: "React", image: "React.png" },
-  { name: "Next.js", image: "Next.js.png" },
-  { name: "Node.js", image: "Node.js.png" },
-  { name: "Git", image: "Git.png" },
-  { name: "Azure DevOps", image: "Azure-Devops.png" },
-  { name: "GitHub", image: "GitHub.png" },
-  { name: "Figma", image: "Figma.png" },
-  { name: "Illustrator", image: "Adobe-Illustrator.png" },
+const techGroups = [
+  {
+    category: "Languages",
+    items: [
+      { name: "HTML", image: "HTML5.png" },
+      { name: "CSS", image: "CSS3.png" },
+      { name: "JavaScript", image: "JavaScript.png" },
+      { name: "TypeScript", image: "TypeScript.png" },
+    ],
+  },
+  {
+    category: "Frameworks & Libraries",
+    items: [
+      { name: "React", image: "React.png" },
+      { name: "Next.js", image: "Next.js.png" },
+      { name: "Node.js", image: "Node.js.png" },
+    ],
+  },
+  {
+    category: "Tools",
+    items: [
+      { name: "Git", image: "Git.png" },
+      { name: "GitHub", image: "GitHub.png" },
+      { name: "Azure DevOps", image: "Azure-Devops.png" },
+    ],
+  },
+  {
+    category: "Design",
+    items: [
+      { name: "Figma", image: "Figma.png" },
+      { name: "Illustrator", image: "Adobe-Illustrator.png" },
+    ],
+  },
 ];
 
 export default function TechStack() {
   return (
     <div className={styles.container}>
-      <h3>Tech Stack</h3>
-      <Text size="xsmall">
-        A look at the languages, frameworks, and tools I reach for when designing, building, and shipping projects.
-      </Text>
-      <div className={styles.techList}>
-        {technologies.map((tech) => (
-          <div key={tech.name} className={styles.techcontainer}>
-            <img
-              src={`/images/tech/${tech.image}`}
-              alt={tech.name}
-              className={styles.icon}
-            />
-            <span>{tech.name}</span>
+      <span className={styles.label}>Tech stack</span>
+      <h2>Technologies I work with</h2>
+      <div className={styles.techGroups}>
+        {techGroups.map((group) => (
+          <div key={group.category} className={styles.techGroup}>
+            <span className={styles.groupLabel}>{group.category}</span>
+            <div className={styles.techList}>
+              {group.items.map((tech) => (
+                <div key={tech.name} className={styles.techcontainer}>
+                  <img
+                    src={`/images/tech/${tech.image}`}
+                    alt={tech.name}
+                    className={styles.icon}
+                  />
+                  <span>{tech.name}</span>
+                </div>
+              ))}
+            </div>
           </div>
         ))}
       </div>
